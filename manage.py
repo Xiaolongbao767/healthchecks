42a2f6723ec038ffdc118f38892010f4cf8ea4f5
hadam94 <hadam@student.bridgew.edu>
2026-04-09 00:21:22 -0400

    Fix api_tokenbucket error by creating admin account (server login now works)

--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@ import logging
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
-
-    from django.core.management import execute_from_command_line
-
     logger = logging.getLogger("django")
+
     try:
         execute_from_command_line(sys.argv)
     except Exception as e:
